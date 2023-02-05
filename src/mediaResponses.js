dc569a0b642d69d9a2a706f622045ced1bb1214a
Jack Walsh <jacksonwalsh931@gmail.com>
2023-02-05 13:15:14 -0500

    eslint will kill me

--- src/mediaResponses.js
+++ src/mediaResponses.js
@@ -2,55 +2,55 @@ const fs = require('fs');
 const path = require('path');
 
 function getMediaStream(request, response, pathInfo, format) {
-  const file = path.resolve(__dirname, pathInfo);
-
-  fs.stat(file, (err, stats) => {
-    if (err) {
-      if (err.code === 'ENOENT') {
-        response.writeHead(404);
+    const file = path.resolve(__dirname, pathInfo);
+  
+    fs.stat(file, (err, stats) => {
+      if (err) {
+        if (err.code === 'ENOENT') {
+          response.writeHead(404);
+        }
+        return response.end(err);
       }
-      return response.end(err);
-    }
-
-    let { range } = request.headers;
-
-    if (!range) {
-      range = 'bytes=0-';
-    }
-
-    const positions = range.replace(/bytes=/, '').split('-');
-
-    let start = parseInt(positions[0], 10);
-
-    const total = stats.size;
-    const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
-
-    if (start > end) {
-      start = end - 1;
-    }
-
-    const chunksize = (end - start) + 1;
-
-    response.writeHead(206, {
-      'Content-Range': `bytes ${start}-${end}/${total}`,
-      'Accept-Range': 'bytes',
-      'Content-Length': chunksize,
-      'Content-Type': format,
+  
+      let { range } = request.headers;
+  
+      if (!range) {
+        range = 'bytes=0-';
+      }
+  
+      const positions = range.replace(/bytes=/, '').split('-');
+  
+      let start = parseInt(positions[0], 10);
+  
+      const total = stats.size;
+      const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+  
+      if (start > end) {
+        start = end - 1;
+      }
+  
+      const chunksize = (end - start) + 1;
+  
+      response.writeHead(206, {
+        'Content-Range': `bytes ${start}-${end}/${total}`,
+        'Accept-Range': 'bytes',
+        'Content-Length': chunksize,
+        'Content-Type': format,
+      });
+  
+      const stream = fs.createReadStream(file, { start, end });
+  
+      stream.on('open', () => {
+        stream.pipe(response);
+      });
+  
+      stream.on('error', (streamErr) => {
+        response.end(streamErr);
+      });
+  
+      return stream;
     });
-
-    const stream = fs.createReadStream(file, { start, end });
-
-    stream.on('open', () => {
-      stream.pipe(response);
-    });
-
-    stream.on('error', (streamErr) => {
-      response.end(streamErr);
-    });
-
-    return stream;
-  });
-}
+  }
 
 const getParty = (request, response) => {
   getMediaStream(request, response, '../client/party.mp4', 'video/mp4');
